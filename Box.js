7cf9ed589617d230e5ff4a37eb68a427a2e9aa55
surya akshaj <96222785+suryaongithub@users.noreply.github.com>
2022-02-10 11:10:23 +0530

    Update Box.js

--- Box.js
+++ Box.js
@@ -11,8 +11,11 @@ class Box{
 
     this.body=Bodies.rectangle(x,y,width,height,option);
     World.add(world,this.body);
-    
+
+    this.ref;
+    this.ref=Math.round(random(1,4));
     }
+    
     display(){
     var pos=this.body.position;
     var angle=this.body.angle;
@@ -20,7 +23,25 @@ class Box{
     translate(pos.x,pos.y);
     rotate(angle);
     rectMode(CENTER);
-    fill("RED");
+    
+    if(this.ref===1)
+    {
+     fill("RED");
+    }
+    else if (this.ref===2) {
+        fill("green");
+    }
+    else if (this.ref===3) 
+    {
+      fill("blue");  
+    }
+    else if(ref===4)
+    {
+       fill("yellow"); 
+    }
+    else {
+        fill("orange")
+    }
     rect(0,0,this.width,this.height);
     pop();
     }
