8acc731f653dc7e0cf70ad53544f679d25dad798
surya akshaj <96222785+suryaongithub@users.noreply.github.com>
2022-02-14 11:06:11 +0530

    Update Box.js
    updated the entire project to make it more customizable

--- Box.js
+++ Box.js
@@ -8,12 +8,11 @@ class Box{
 
     this.width=width;
     this.height=height;
-
     this.body=Bodies.rectangle(x,y,width,height,option);
+    this.color=anyOne(["red","green","yellow","white","black","magenta","pink"])
     World.add(world,this.body);
 
-    this.ref;
-    this.ref=Math.round(random(1,4));
+    
     }
     
     display(){
@@ -23,26 +22,10 @@ class Box{
     translate(pos.x,pos.y);
     rotate(angle);
     rectMode(CENTER);
-    
-    if(this.ref===1)
-    {
-     fill("RED");
-    }
-    else if (this.ref===2) {
-        fill("green");
-    }
-    else if (this.ref===3) 
-    {
-      fill("blue");  
-    }
-    else if(ref===4)
-    {
-       fill("yellow"); 
-    }
-    else {
-        fill("orange")
-    }
+    fill(this.color);
     rect(0,0,this.width,this.height);
     pop();
+
+    
     }
     }
