88965290cfea7408ae410583252297a5212648ef
surya akshaj <96222785+suryaongithub@users.noreply.github.com>
2022-02-14 11:47:37 +0530

    Update Box.js

--- Box.js
+++ Box.js
@@ -1,15 +1,38 @@
 class Box{
     constructor(x,y,width,height){
 
-    var option=
+     this.option=
     {
     restitution:0.8
     }
 
+    this.colors=
+    [
+     "red",
+     "green",
+     "yellow",
+     "white",
+     "black",
+     "magenta",
+     "pink",
+     "blue",
+     "brown",
+     "grey",
+     "purple",
+     "lime",
+     "silver",
+     "gold",
+     "maroon",
+     "aqua",
+     "teal",
+     "olive",
+     "navy"
+    ]
+
     this.width=width;
     this.height=height;
-    this.body=Bodies.rectangle(x,y,width,height,option);
-    this.color=anyOne(["red","green","yellow","white","black","magenta","pink"])
+    this.body=Bodies.rectangle(x,y,width,height,this.option);
+    this.color=anyOne(this.colors)
     World.add(world,this.body);
 
     
